3ae1f0aefa3c7e2d96f5b5e371bfc408cd63fdb3
Théo Potier <48621976+Nitr0z@users.noreply.github.com>
2022-09-22 23:28:22 +0200

    finish bot

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on('ready', () => {
   // regarder toutes les 5 mins s'il y à une nouvelle transaction sur l'adresse 0x4510ef604e0595f7151adcba0b958d39b8b16d40
 //  setInterval(async function(){
     var subscription = web3.eth.subscribe('logs', {
-      address: '0x28AaF29992B43e093A7D883c7427bd1fF4a6665b',
+      address: '0x4510Ef604e0595F7151aDCBA0B958d39b8B16D40',
   });
 
   subscription.on('data', event => {
@@ -71,19 +71,19 @@ client.on('ready', () => {
 
         // récuperer le prix de la transaction
         web3.eth.getTransaction(event.transactionHash).then((transaction) => {
-            const channel = client.channels.cache.get('854840063988203570');
+            const channel = client.channels.cache.get('739518433779122191');
             const embed8 = new Discord.MessageEmbed()
             // mettre le titre de l'embed avec l'id du token
             .setTitle('** Dark Taverns #'+tokenId+'** has been sold !')
             // ajout d'une url vers opensea avec le token id
-            .setURL('https://opensea.io/assets/0x28aaF29992B43e093A7D883c7427bd1fF4a6665b/'+tokenId)
+            .setURL('https://opensea.io/assets/0x4510Ef604e0595F7151aDCBA0B958d39b8B16D40/'+tokenId)
             // display price in eth and usd
             .addFields(
-                { name: 'Price : ' , value: '' + web3.utils.fromWei(transaction.value, 'ether') + ' ETH ($)'},)
-            .addFields(
-                { name: 'From : ', value: '' +from+ ''},)
-            .addFields(
-                { name: 'To : ', value: '' +to+ ''},)
+                { name: 'Item : ', value: 'Dark Taverns #'+tokenId+''},
+                { name: 'Price : ' , value: '' + web3.utils.fromWei(transaction.value, 'ether') + ' ETH ($)'},
+                { name: 'From : ', value: '' +from+ '', inline: true},
+                { name: 'To : ', value: '' +to+ '', inline: true},
+            )
             // ajouter l'image 
             .setImage('https://images-ext-1.discordapp.net/external/6duDU5N4AmMfhbPRK75syzwwXqjWVbPtwgI9oHQE6o0/https/miro.medium.com/max/1200/1%2AgUTWfOQ5baxs4H8NJ-Z1vA.png?width=671&height=671')
             // footer afficher nom collection & date du jour en anglais
