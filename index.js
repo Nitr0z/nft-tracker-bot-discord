0d03a2c53f7883271524310a26113be144822888
Théo Potier <48621976+Nitr0z@users.noreply.github.com>
2022-09-23 23:29:25 +0200

    add img support

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const web3 = new Web3('wss://mainnet.infura.io/ws/v3/your_infura_key');
  const collectionAdress = ""; // exemple : 0x4510Ef604e0595F7151aDCBA0B958d39b8B16D40  | is Dark Taverns collection
  const collectionName = "";  // exemple : Dark Taverns
  const discordChannel = ""; // exemple : 739518433779122191
-// if your collection have mp4 we can't put in discord so we put a static img
- const collectionImg = "";
 
 
 
@@ -55,30 +53,83 @@ client.on('ready', () => {
         var to = transaction.to;
         var tokenId = transaction.tokenId;
 
-        // with token id, get the token url 
-
-        // get the transaction price (eth ou weth) 
-
-        // get the transaction price
-        web3.eth.getTransaction(event.transactionHash).then((transaction) => {
-            var price = web3.utils.fromWei(transaction.value, 'ether');
-                // print the transaction on discord
-                const channel = client.channels.cache.get(discordChannel);
-                const embed8 = new Discord.MessageEmbed()
-                .setTitle('**'+collectionName+' #'+tokenId+'** has been sold !')
-                .setURL('https://opensea.io/assets/'+collectionAdress+'/'+tokenId)
-                .addFields(
-                    { name: 'Item : ', value: collectionName+' #'+tokenId+''},
-                    { name: 'Price : ' , value: '' + price + ' ETH ($)'},
-                    { name: 'From : ', value: '['+from.substring(0, 6)+'...'+from.substring(38, 42)+'](https://opensea.io/accounts/'+from+')', inline: true},
-                    { name: 'To : ', value: '['+to.substring(0, 6)+'...'+from.substring(38, 42)+'](https://opensea.io/accounts/'+to+')', inline: true},
-                )
-                .setImage(collectionImg)
-                .setFooter(collectionName+' | '+new Date().toLocaleDateString('en-EN', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }))
-                .setColor('RANDOM')
-                channel.send(embed8)
-        });
-    }
-})
-  
-  
+                // with token id, get the token url 
+                const tokenURIABI = [
+                    {
+                        "inputs": [
+                            {
+                                "internalType": "uint256",
+                                "name": "tokenId",
+                                "type": "uint256"
+                            }
+                        ],
+                        "name": "tokenURI",
+                        "outputs": [
+                            {
+                                "internalType": "string",
+                                "name": "",
+                                "type": "string"
+                            }
+                        ],
+                        "stateMutability": "view",
+                        "type": "function"
+                    }
+                ];
+                
+                const tokenContract = collectionAdress 
+                const tokenId2 = tokenId
+                
+                const contract = new web3.eth.Contract(tokenURIABI, tokenContract)
+                
+                async function getNFTMetadata() {
+                    const result = await contract.methods.tokenURI(tokenId2).call()
+                
+                    console.log(result); // ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/101
+                
+                    
+                    // get the image_url in json from result
+                    const url = result;
+                    axios.get(url)
+                        .then(response => {
+                            var img = response.data.image;
+        
+                            // si img commence par ipfs:// alors on le transforme en https://cloudflare-ipfs.com/ipfs/
+                            if (img.startsWith("ipfs://")) {
+                                img = img.replace("ipfs://", "https://cloudflare-ipfs.com/ipfs/"+img.substring(7));
+                            } else {
+                                // do nothing
+                            }
+        
+                            // get the transaction price (eth ou weth) 
+        
+                            // get the transaction price
+                            web3.eth.getTransaction(event.transactionHash).then((transaction) => {
+                                var price = web3.utils.fromWei(transaction.value, 'ether');
+                                    // print the transaction on discord
+                                    const channel = client.channels.cache.get(discordChannel);
+                                    const embed8 = new Discord.MessageEmbed()
+                                    .setTitle('**'+collectionName+' #'+tokenId+'** has been sold !')
+                                    .setURL('https://opensea.io/assets/'+collectionAdress+'/'+tokenId)
+                                    .addFields(
+                                        { name: 'Item : ', value: collectionName+' #'+tokenId+''},
+                                        { name: 'Price : ' , value: '' + price + ' ETH ($)'},
+                                        { name: 'From : ', value: '['+from.substring(0, 6)+'...'+from.substring(38, 42)+'](https://opensea.io/accounts/'+from+')', inline: true},
+                                        { name: 'To : ', value: '['+to.substring(0, 6)+'...'+from.substring(38, 42)+'](https://opensea.io/accounts/'+to+')', inline: true},
+                                    )
+                                    .setImage(img)
+                                    .setFooter(collectionName+' | '+new Date().toLocaleDateString('en-EN', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }))
+                                    .setColor('RANDOM')
+                                    channel.send(embed8)
+                            });
+                        })
+                }
+        
+                getNFTMetadata();
+        
+        
+        
+            }
+        })
+          
+          
+        
